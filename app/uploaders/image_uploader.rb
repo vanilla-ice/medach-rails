4bc529503f8a4bbdfa9044ce74efbf701074f1e7
halla <hallamahyn@gmail.com>
2017-11-06 13:34:28 +0300

    vse robit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -23,16 +23,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
+  process scale: [200, 300]
+  
+  def scale(width, height)
+    # do something
+  end
 
   # Create different versions of your uploaded files:
-  version :large do
-    process resize_to_fit: [300, 300]
-  end
+  # version :large do
+  #   process resize_to_fit: [300, 300]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
