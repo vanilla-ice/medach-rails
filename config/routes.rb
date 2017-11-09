810f05b8e0d168603d5fa89ebf75337ef156c7e2
halla <hallamahyn@gmail.com>
2017-11-09 18:25:20 +0300

    add route for show all tags

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   namespace :api do
-    resources :articles
-    get "/articles/by_tag/:tag_name" => "articles#get_by_tag", :as => :articles_by_tag
+    resources :articles do
+      collection do
+        get 'all_tags'
+      end
+      member do
+        get 'by_tag/:tag_name', action: :by_tag, as: :by_tag
+      end
+    end
   end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
