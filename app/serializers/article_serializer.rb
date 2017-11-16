d7d0a234636203e69144ef710f507ce8f7b94c8d
halla <hallamahyn@gmail.com>
2017-11-16 18:34:41 +0300

    fix problem by shorttext

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,5 +1,5 @@
 class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :body, :image, :title, :author, :infographic, :redaction, :created_at, :tag_list
+  attributes :id, :body, :image, :title, :author, :infographic, :redaction, :created_at, :tag_list, :shorttext
 
   def tag_list
     object.tag_list
