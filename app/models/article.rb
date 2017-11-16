63fe6b2560e80a482263f57f3b6c8ff454ba8d42
halla <hallamahyn@gmail.com>
2017-11-16 18:34:41 +0300

    fix problem by shorttext

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   include PgSearch
-  multisearchable :against =>[:body, :title, :author, :infographic, :redaction]
-  pg_search_scope :search, :against => [:body, :title, :author, :infographic, :redaction],
+  multisearchable :against =>[:body, :title, :author, :infographic, :redaction, :shorttext]
+  pg_search_scope :search, :against => [:body, :title, :author, :infographic, :redaction, :shorttext],
                              :using => {
                                :tsearch => {:dictionary => "russian", :prefix => true}
                               }
