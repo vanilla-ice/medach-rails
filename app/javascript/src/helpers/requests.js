e16c3f9e7fa981b1f27b9477fce86ecfeafa07c3
vanilla-ice <valeraerohin97@mail.ru>
2017-11-22 20:01:53 +0300

    Fix: base url

--- app/javascript/src/helpers/requests.js
+++ app/javascript/src/helpers/requests.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
  
-const BASE_URL = "http://localhost:3000"
+const BASE_URL = "https://medach-project.herokuapp.com"
 
 export const simpleRequest = (url, method, data, headers) => {
   const requestObj = {
