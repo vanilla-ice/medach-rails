065c8f75e773887b954a74fcb7a83185337d1a55
halla <hallamahyn@gmail.com>
2017-11-29 16:57:25 +0300

    Fix some problem

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -3,48 +3,49 @@
 //= require activeadmin/quill_editor_input
 
 
-var IMGUR_CLIENT_ID = 'bcab3ce060640ba';
-var IMGUR_API_URL = 'https://api.imgur.com/3/image';
+// var IMGUR_CLIENT_ID = 'bcab3ce060640ba';
+// var IMGUR_API_URL = 'https://api.imgur.com/3/image';
 
-function imageHandler(image, callback) {
-  var data = new FormData();
-  data.append('image', image);
+// function imageHandler(image, callback) {
+//   var data = new FormData();
+//   data.append('image', image);
 
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', IMGUR_API_URL, true);
-  // xhr.setRequestHeader('Authorization', 'Client-ID ' + IMGUR_CLIENT_ID);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-      var response = JSON.parse(xhr.responseText);
-      if (response.status === 200 && response.success) {
-        callback(response.data.link);
-      } else {
-        var reader = new FileReader();
-        reader.onload = function(e) {
-          callback(e.target.result);
-        };
-        reader.readAsDataURL(image);
-      }
-    }
-  }
-  xhr.send(data);
-}
+//   var xhr = new XMLHttpRequest();
+//   xhr.open('POST', IMGUR_API_URL, true);
+//   // xhr.setRequestHeader('Authorization', 'Client-ID ' + IMGUR_CLIENT_ID);
+//   xhr.onreadystatechange = function() {
+//     if (xhr.readyState === 4) {
+//       var response = JSON.parse(xhr.responseText);
+//       if (response.status === 200 && response.success) {
+//         callback(response.data.link);
+//       } else {
+//         var reader = new FileReader();
+//         reader.onload = function(e) {
+//           callback(e.target.result);
+//         };
+//         reader.readAsDataURL(image);
+//       }
+//     }
+//   }
+//   xhr.send(data);
+// }
 
-document.addEventListener("DOMContentLoaded", function() {
-  var quill = new Quill('#article_body', {
-    modules: {
-      toolbar: [['bold', 'italic', 'underline', 'strike'],
-      ['link', 'image'],
-      [{ 'size': ['small', false, 'large', 'huge'] }],
-      [{ 'script': 'sub'}, { 'script': 'super' }],
-      [{ 'direction': 'rtl' }] ,
-      ['blockquote', 'code-block'] ,
-      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-      [{ 'indent': '-1'}, { 'indent': '+1' }],
-      ['clean']]
+// // document.addEventListener("DOMContentLoaded", function() {
+// //   var quill = new Quill('#article_body', {
+// //     modules: {
+// //       toolbar: [['bold', 'italic', 'underline', 'strike'],
+// //       ['link', 'image'],
+// //       [{ 'size': ['small', false, 'large', 'huge'] }],
+// //       [{ 'script': 'sub'}, { 'script': 'super' }],
+// //       [{ 'direction': 'rtl' }] ,
+// //       ['blockquote', 'code-block'] ,
+// //       [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+// //       [{ 'indent': '-1'}, { 'indent': '+1' }],
+// //       ['clean']]
 
-    },
-    theme: 'snow',
-    imageHandler: imageHandler
-  });
-});
+//     },
+//     theme: 'snow',
+//     imageHandler: imageHandler
+//   });
+// });
+
