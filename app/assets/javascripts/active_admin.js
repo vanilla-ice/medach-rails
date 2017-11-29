fc599da92ccdd9d305e9fd5dba5428db214c1f6e
halla <hallamahyn@gmail.com>
2017-11-29 11:57:50 +0300

    add quill editor

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,3 +1,50 @@
 //= require active_admin/base
-//= require activeadmin/trumbowyg/trumbowyg
-//= require activeadmin/trumbowyg_input
+//= require activeadmin/quill_editor/quill
+//= require activeadmin/quill_editor_input
+
+
+var IMGUR_CLIENT_ID = 'bcab3ce060640ba';
+var IMGUR_API_URL = 'https://api.imgur.com/3/image';
+
+function imageHandler(image, callback) {
+  var data = new FormData();
+  data.append('image', image);
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', IMGUR_API_URL, true);
+  // xhr.setRequestHeader('Authorization', 'Client-ID ' + IMGUR_CLIENT_ID);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      var response = JSON.parse(xhr.responseText);
+      if (response.status === 200 && response.success) {
+        callback(response.data.link);
+      } else {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+          callback(e.target.result);
+        };
+        reader.readAsDataURL(image);
+      }
+    }
+  }
+  xhr.send(data);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var quill = new Quill('#article_body', {
+    modules: {
+      toolbar: [['bold', 'italic', 'underline', 'strike'],
+      ['link', 'image'],
+      [{ 'size': ['small', false, 'large', 'huge'] }],
+      [{ 'script': 'sub'}, { 'script': 'super' }],
+      [{ 'direction': 'rtl' }] ,
+      ['blockquote', 'code-block'] ,
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      [{ 'indent': '-1'}, { 'indent': '+1' }],
+      ['clean']]
+
+    },
+    theme: 'snow',
+    imageHandler: imageHandler
+  });
+});
