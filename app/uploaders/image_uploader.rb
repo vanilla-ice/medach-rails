43859a69faf95232a6fdeeb372df7307a007ce51
Kirill Kuznetsov <hallamahyn@gmail.com>
2018-02-14 19:56:29 +0300

    add rake tast for add some tags for articles

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,10 @@
 class ImageUploader < CommonUploader
-  # process scale: [200, 300]
+  include CarrierWave::MiniMagick
+  # process scale: [278, 261]
+  
+  version :thumb do
+    process resize_to_fill: [278,261]
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
