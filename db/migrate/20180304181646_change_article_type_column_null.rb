eda0ba3f0a6b3682cf87c4217fa8be7c337eeda5
Bogdan Kovtun <nemo48@yandex.ru>
2018-03-12 13:22:45 +0300

    Change migration to update all existed articles

--- db/migrate/20180304181646_change_article_type_column_null.rb
+++ db/migrate/20180304181646_change_article_type_column_null.rb
@@ -1,5 +1,10 @@
 class ChangeArticleTypeColumnNull < ActiveRecord::Migration[5.1]
+  class Article < ActiveRecord::Base
+  end
+
   def change
     change_column_null :articles, :type, false, "LongreadArticle"
+    Article.reset_column_information
+    Article.update_all(type: 'LongreadArticle')
   end
 end
