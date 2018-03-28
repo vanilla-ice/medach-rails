a3cbd4d45752053eab7b0e7be961dfd2178d56c3
vanilla-ice <valeraerohin97@mail.ru>
2018-03-28 15:20:33 +0300

    WIP: quickview

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -74,6 +74,11 @@ function insertToEditor(url, editor) {
 
 }
 
+var renderQuickviewPopup = function renderQuickviewPopup(data) {	
+  var result = '<div class="quickview-popup">' + '<div class="js-close close-popup">Закрыть</div>' + '<div class="main main-description">' + '<div class="container">' + '<div class="foto-column">' + '<div class="foto-column__poster">' + '<img src="' + data.image + '"/>' + ' </div>' + '</div>' + '<div class="desc-column ql-editor">' + '<div class="desc-column__title">' + data.name + '</div>' + '<div class="desc-column__text">' + data.post + '</div>' + '</div>' + '<div class="info-column">' + '<div class="info-column__item">Автор: ' + data.author + '</div>' + '<div class="info-column__item info-column__infographic">Инфографика: <a href="' + data.infographic + '">' + data.infographic + '</a></div>' + '<div class="info-column__item">Редакция: ' + data.redaction + '</div>' + '<div class="info-column__item info-column__infographic">Оригинал: <a href="' + data.origin + '">' + data.origin + '</a></div>' + '</div>' + '</div>' + '</div>' + '</div>';	
+  return result;	
+};
+
 $(document).ready(function () {
   var editor;
 
@@ -95,7 +100,81 @@ $(document).ready(function () {
     paste: {
       style: 'clean',  // Overrides default: 'prompt' for MS Office content
       enableFlashImport: true // Note, true is the default
-  }
+    },
+    ui: {
+      toolbar: {
+        items: [
+          {
+            label: 'undo',
+            items: ['undo', 'redo']
+          },
+          {
+            label: 'insert',
+            id: 'insert',
+            items: ['link', 'fileupload', 'table', 'media', 'hr', 'specialchar']
+          },
+          'style',
+          {
+            label: 'emphasis',
+            items: ['bold', 'italic', 'underline']
+          },
+          {
+            label: 'align',
+            items: ['alignment']
+          },
+          {
+            label: 'format',
+            items: ['font-menu', 'removeformat']
+          },
+          {
+            label: 'tools',
+            items: ['find', 'accessibility', 'fullscreen', 'usersettings']
+          },
+          {
+            label: 'Quickview',
+            items: [
+              {
+                id: 'quickview1',
+                text: 'Открыть предпросмотр',
+
+                action: function() {
+                  
+                  $('body').css('overflow-y', 'hidden');	
+                  var data = {};	
+                
+                  data['name'] = $('#article_title').val();	
+                  data['post'] = $('.ql-editor').html();	
+                  data['redaction'] = $('#article_redaction').val();	
+                  data['author'] = $('#article_author').val();	
+                  data['infographic'] = $('#article_infographic').val();	
+                  data['origin'] = $('#article_origin').val();	
+                  data['image'] = $('#article_image_url').val();	
+                  $('body').append(renderQuickviewPopup(data));	
+                
+                  $('body').on('click', '.js-close', function () {	
+                    $('.popup-wrapper > div').remove();	
+                    $('body').css('overflow-y', 'initial');	
+                  });	
+                }
+              }
+            ]
+          },
+          {
+            label: 'FileLoader',
+            items: [
+              {
+                id: 'fileLoaderPdf',
+                text: 'Загрузить файл',
+
+                action: function() {
+                  return console.log('file loader')
+                }
+              }
+            ]
+          },
+        ]
+      }
+    }
   };
 
   if (document.getElementById('article_body')) {
@@ -107,5 +186,4 @@ $(document).ready(function () {
     no_results_text: 'No results matched',
     width: '80%'
   })
-
 });
