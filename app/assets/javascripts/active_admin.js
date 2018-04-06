3bba190a9018f7dfc185bdff2fca2fc84e18c823
Мак <mak@MacBook-Pro-Mak.local>
2018-04-06 18:36:12 +0300

    /images

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -190,7 +190,7 @@ $(document).ready(function () {
               {
                 id: 'quickview1',
                 text: 'Открыть предпросмотр',
-                icon: '/assets/preview.png',
+                icon: '/images/preview.png',
 
                 action: function(e) {
                   
@@ -221,7 +221,7 @@ $(document).ready(function () {
               {
                 id: 'fileLoaderPdf',
                 text: 'Загрузить файл',
-                icon: '/assets/upload-file.svg',
+                icon: '/images/upload-file.svg',
 
                 action: function() {
                   selectLocalDocument(function (data) {
