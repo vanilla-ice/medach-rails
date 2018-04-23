96a67948a9d038d09f163d8fd1701fd09308d979
vanilla-ice <valeraerohin97@mail.ru>
2018-04-23 20:44:54 +0300

    Fix: add styles for editor

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -135,7 +135,7 @@ $(document).ready(function () {
   var config = {
     basePath: '/textboxio',
     css : {
-      stylesheets : ['/assets/editor.css'] // an array of CSS file URLs
+      stylesheets : ['/css/editor.css'] // an array of CSS file URLs
     },
     images: {
       upload: {
