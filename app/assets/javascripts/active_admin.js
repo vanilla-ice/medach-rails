9912080601074900a74a0015d9f784c233d2a33c
vanilla-ice <valeraerohin97@mail.ru>
2018-08-06 15:22:06 +0300

    WIP: save draft

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -228,6 +228,35 @@ $(document).ready(function () {
             ]
           },
           {
+            label: 'set backup',
+            items: [
+              {
+                id: 'setBackupData',
+                text: 'Сделать бекап',
+                icon: '/images/save-icon.svg',
+
+                action: function(e) {
+                  localStorage.setItem('postData', e.content.get())
+                }
+              }
+            ]
+          },
+          {
+            label: 'get backup',
+            items: [
+              {
+                id: 'getBackupData',
+                text: 'Использовать последний бекап',
+                icon: '/images/preview.png',
+
+                action: function(e) {
+                  var data = localStorage.getItem('postData');
+                  e.content.set(data)
+                }
+              }
+            ]
+          },
+          {
             label: 'FileLoader',
             items: [
               {
@@ -270,4 +299,6 @@ $(document).ready(function () {
     no_results_text: 'No results matched',
     width: '80%'
   })
+
+
 });
