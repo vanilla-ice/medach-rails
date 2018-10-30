955b7b980a2cf234c3ed35ed3b34560e68496dda
Egor Romanov <unsaidxpl@gmail.com>
2018-10-30 14:01:24 +0300

    change frontend prod script

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,7 +48,7 @@ task :yarn do
 end
 
 task :setup_frontend do
-  command %{cd medach_frontend && yarn install && yarn build}
+  command %{cd medach_frontend && yarn install && yarn build && yarn prod}
 end
 
 desc "Deploys the current version to the server."
