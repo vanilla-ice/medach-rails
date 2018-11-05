de75491ac7e94443904d4dbaecf96b67281d258a
Egor Romanov <unsaidxpl@gmail.com>
2018-11-05 12:43:14 +0300

    update deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :application_name, 'medach'
 set :domain, '142.93.13.232'
 set :deploy_to, '/home/deploy/apps/medach'
 set :repository, 'git://github.com/vanilla-ice/medach-rails.git'
-set :branch, 'develop'
+set :branch, 'master'
 
 # Optional settings:
 #   set :user, 'foobar'          # Username in the server to SSH to.
@@ -48,7 +48,8 @@ task :yarn do
 end
 
 task :setup_frontend do
-  command %{cd medach_frontend && yarn install && yarn build && yarn prod}
+  command %{cd medach_frontend && yarn install}
+  command %{cd yarn build && pm2 kill && yarn prod}
 end
 
 desc "Deploys the current version to the server."
