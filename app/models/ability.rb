588d4ab2b5c01ba46251006ed5ba629420640e3d
Egor Romanov <unsaidxpl@gmail.com>
2018-11-08 17:07:14 +0300

    fix regular user abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,9 @@ class Ability
   def initialize(user)
     if user
       can :read, ActiveAdmin::Page, name: "Dashboard"
-      can :read, Article
-      can [:update, :show], User do |u|
+      can [:read, :create], Article
+      can :read, User
+      can :update, User do |u|
         u.id == user.id
       end
       can [:update, :edit], Article do |a|
