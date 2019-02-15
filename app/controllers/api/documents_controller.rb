ff65a01884f03018204d8e860d0aea08cb1cc5b5
Kim Konstantin <kimkonsts@gmail.com>
2019-02-15 15:36:22 +0300

    Fix: parse documents_controller params

--- app/controllers/api/documents_controller.rb
+++ app/controllers/api/documents_controller.rb
@@ -2,9 +2,9 @@ module Api
   class DocumentsController < BaseController
     respond_to  :json
     def create
-      @document = Document.new(url: params[:document])
+      @document = Document.new(url: params[:document] || params[:file].last)
       if @document.save!
-        render json: { filekey: { url: @document.url, name: @document.name, id: @document.id } }
+        render json: { filekey: { url: @document.url.url, name: @document.name, id: @document.id } }
       else
         render json: {ok: false}, status: 422
       end
