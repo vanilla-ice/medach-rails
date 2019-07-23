d63de0f9c1f0bdcdba8d341498390b80e7485eee
Kim Konstantin <kimkonsts@gmail.com>
2019-07-23 14:50:15 +0300

    Fix document upload

--- app/controllers/api/documents_controller.rb
+++ app/controllers/api/documents_controller.rb
@@ -2,12 +2,35 @@ module Api
   class DocumentsController < BaseController
     respond_to  :json
     def create
-      @document = Document.new(url: params[:document])
-      if @document.save!
-        render json: { filekey: { url: @document.url, name: @document.name, id: @document.id } }
+      if file_params.count > 1
+        response = []
+        file_params.each_with_index do |file, i|
+          @document = Document.new(url: file)
+          if @document.save!
+            name = @document&.name.present?  ? @document.name : /[^\/]+$/.match(@document.url.url).to_s
+            hs = { "file-#{i+1}".to_sym => { url: @document.url.url, name: name, id: @document.id } }
+            response << hs
+          else
+            render json: { ok: false }, status: 422
+          end
+        end
+
+        render json: response
       else
-        render json: { ok: false }, status: 422
+        @document = Document.new(url: file_params.last)
+        if @document.save!
+          name = @document&.name.present?  ? @document.name : /[^\/]+$/.match(@document.url.url).to_s
+          render json: { file: {url: @document.url.url, name: name, id: @document.id } }
+        else
+          render json: { ok: false }, status: 422
+        end
       end
+    end
+
+    private
+
+    def file_params
+      params.require(:file)
     end
   end
 end
