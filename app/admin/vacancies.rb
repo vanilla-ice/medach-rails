7ce70ed8f315b4caf20cbc4d25630b5cb40d5e87
Kim Konstantin <kimkonsts@gmail.com>
2020-04-14 16:13:53 +0300

    Create AA views for Vacancy

---
+++ app/admin/vacancies.rb
@@ -0,0 +1,59 @@
+ActiveAdmin.register Vacancy do
+  permit_params(
+    :title,
+    :employer,
+    :location,
+    :experience,
+    :contacts,
+    :is_promoted,
+    :is_approved,
+    :external_url,
+    :content
+  )
+
+  scope 'Все', :all
+  scope ('Неподтвержденные') { |scope| scope.where(is_approved: false) }
+
+  index do
+    selectable_column
+    id_column
+    column 'Заголовок', :title do |v|
+      link_to v.title, admin_vacancy_path(v)
+    end
+    column :employer
+    column :location
+    column :experience
+    column :contacts
+    column :is_promoted
+    column :is_approved
+  end
+
+  filter :created_at
+
+  form do |f|
+    inputs do
+      input :title
+      input :employer
+      input :location
+      input :experience
+      input :content, label: 'Текст вакансии', :input_html => { id: :article_body }
+      input :contacts
+      input :is_promoted
+      input :is_approved
+      input :external_url
+    end
+
+    actions
+  end
+
+  show do
+    attributes_table do
+      default_attribute_table_rows.each do |field|
+        row(field)
+      end
+    end
+
+    active_admin_comments
+  end
+
+end
