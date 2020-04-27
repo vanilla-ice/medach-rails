b57df16bc7e84f102606664d46568d4cd39803af
Kim Konstantin <kimkonsts@gmail.com>
2020-04-27 21:38:55 +0300

    hotfix

--- app/controllers/api/documents_controller.rb
+++ app/controllers/api/documents_controller.rb
@@ -2,22 +2,32 @@ module Api
   class DocumentsController < BaseController
     respond_to  :json
     def create
-      if file_params.count > 1
-        response = []
-        file_params.each_with_index do |file, i|
-          @document = Document.new(url: file)
+      if file_params.try(:count)
+        if file_params.count > 1
+          response = []
+          file_params.each_with_index do |file, i|
+            @document = Document.new(url: file)
+            if @document.save!
+              name = @document&.name.present?  ? @document.name : /[^\/]+$/.match(@document.url.url).to_s
+              hs = { "file-#{i+1}".to_sym => { url: @document.url.url, name: name, id: @document.id } }
+              response << hs
+            else
+              render json: { ok: false }, status: 422
+            end
+          end
+
+          render json: response
+        else
+          @document = Document.new(url: (file_params.try(:last) || file_params))
           if @document.save!
             name = @document&.name.present?  ? @document.name : /[^\/]+$/.match(@document.url.url).to_s
-            hs = { "file-#{i+1}".to_sym => { url: @document.url.url, name: name, id: @document.id } }
-            response << hs
+            render json: { file: {url: @document.url.url, name: name, id: @document.id } }
           else
             render json: { ok: false }, status: 422
           end
         end
-
-        render json: response
       else
-        @document = Document.new(url: file_params.last)
+        @document = Document.new(url: (file_params.try(:last) || file_params))
         if @document.save!
           name = @document&.name.present?  ? @document.name : /[^\/]+$/.match(@document.url.url).to_s
           render json: { file: {url: @document.url.url, name: name, id: @document.id } }
