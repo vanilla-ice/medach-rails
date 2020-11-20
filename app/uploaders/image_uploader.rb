4ea4fc067501019e1dc9ae59c374157508db7dab
Alexandr Tarelkin <tarelkinaa@bk.ru>
2020-11-20 23:15:19 +0300

    images optimization

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,11 +7,26 @@ class ImageUploader < CommonUploader
   #   process resize_to_fill: [278,261]
   # end
 
+  process :optimize
+
   def store_dir
     if model.class.to_s.in?(Article::TYPES)
       return "uploads/article/#{mounted_as}/#{model.id}"
     end
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def optimize
+    manipulate! do |img|
+        return img unless img.mime_type.match /image\/jpeg/
+        img.strip
+        img.combine_options do |c|
+            c.quality "80"
+            c.depth "8"
+            c.interlace "plane"
+        end
+        img
+    end
   end
 
   def extension_whitelist
